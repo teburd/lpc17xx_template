d43e95401783551f473ffe891458f368bfe47624
Tom Burdick <tburdick@wrightwoodtech.com>
2011-08-02 09:33:54 -0500

    remove install target, not needed for firmware project

--- rules.mk
+++ rules.mk
@@ -55,22 +55,6 @@ targets:	$(TGT_BIN) $(TGT_SBIN) $(TGT_ETC) $(TGT_LIB)
 clean:
 		rm -f $(CLEAN)
 
-.PHONY:		install
-install:	targets 
-		$(INST) $(TGT_BIN) -m 755 -d $(DIR_BIN)
-		$(CMD_INSTBIN)
-		$(INST) $(TGT_SBIN) -m 750 -d $(DIR_SBIN)
-		$(CMD_INSTSBIN)
-ifeq ($(wildcard $(DIR_ETC)/*),)
-		$(INST) $(TGT_ETC) -m 644 -d $(DIR_ETC)
-		$(CMD_INSTETC)
-else
-		@echo Configuration directory $(DIR_ETC) already present -- skipping
-endif
-		$(INST) $(TGT_LIB) -m 750 -d $(DIR_LIB)
-		$(CMD_INSTLIB)
-
-
 # Prevent make from removing any build targets, including intermediate ones
 
 .SECONDARY:	$(CLEAN)
